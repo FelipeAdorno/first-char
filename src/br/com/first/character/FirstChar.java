e53ad0044a932a761fb0f926814bdd75193f609a
Felipe Adorno <felipeadsc@gmail.com>
2015-11-02 14:30:04 -0200

    Remove unused

--- src/br/com/first/character/FirstChar.java
+++
@@ -1,7 +0,0 @@
-package br.com.first.character;
-
-/**
- * Created by felipe on 02/11/15.
- */
-public class FirstChar {
-}
--- src/br/com/first/character/exceptions/FirstUniqueCharNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package br.com.first.character.exceptions;
-
-public class FirstUniqueCharNotFoundException extends RuntimeException {
-
-    public FirstUniqueCharNotFoundException() {
-        super("Unique character not found in stream!");
-    }
-
-}
